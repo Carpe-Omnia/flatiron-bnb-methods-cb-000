60985e0517d7338091faad6398932dca871c7683
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-01 18:15:42 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -15,8 +15,8 @@ class City < ActiveRecord::Base
     emray = []
     self.listings.each do |listing|
       occupied = listing.reservations.any? do |res|
-        c_in = res.checkin.to_s.intize
-        c_out = res.checkout.to_s.intize
+        c_in = intize(res.checkin.to_s)
+        c_out = intize(res.checkout.to_s)
         c_in > start && c_in < stop || c_out > start && c_out < stop || c_in < start && c_out > stop
       end
       if occupied == false
