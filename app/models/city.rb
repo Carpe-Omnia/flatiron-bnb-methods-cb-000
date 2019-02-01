b95f6120d8a03b01a3af34e3fa5108d7d880e46a
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-01 18:09:13 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -10,14 +10,20 @@ class City < ActiveRecord::Base
 
 
   def city_openings(startdate, stopdate)
-    startray = start.split("-")
-    stopray = stop.split("-")
-    startray.each do |thing|
-      thing = thing.to_i
-    end
-    stopray.each do |thing|
-      thing = thing.to_i
-    end
-
+    start = intize(startdate)
+    stop = intize(stopdate)
+    emray = []
+    self.listings.each do |listing|
+      occupied = listing.reservations.any? do |res|
+        c_in = res.checkin.to_s.intize 
+        c_out = res.checkout.to_s.intize 
+        c_in > start && c_in < stop || c_out > start && c_out < stop || c_in < start && c_out > stop 
+      end 
+      if occupied == false 
+        emray << listing 
+      end 
+    end 
+    emray 
+  end       
 
 end
