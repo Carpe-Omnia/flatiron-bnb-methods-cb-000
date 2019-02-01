8f23d61176fa368500f09797bdbb7d609e356af4
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-01 17:43:49 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -2,11 +2,10 @@ class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
-  
-  def city_openings(start, stop) 
+
+  def city_openings(start, stop)
     startray = start.split("-")
     stopray = stop.split("-")
 
-    
-end
 
+end
