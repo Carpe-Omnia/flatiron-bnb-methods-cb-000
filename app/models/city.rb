0bee80c2fdd95cce1b46474c28f32a652e50e11d
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-01 18:56:08 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -7,6 +7,7 @@ end
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
+  has_many :reservations, :through => :listings
 
 
   def city_openings(startdate, stopdate)
