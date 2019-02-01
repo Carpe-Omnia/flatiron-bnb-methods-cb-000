a2ef2a1ae088dbeff0d310c9f96f45709e445a2b
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-01 19:23:12 +0000

    Automatically backed up by Learn

--- app/models/neighborhood.rb
+++ app/models/neighborhood.rb
@@ -7,6 +7,17 @@ class Neighborhood < ActiveRecord::Base
     start = intize(startdate)
     stop = intize(stopdate)
     emray = []
+    self.listings.each do |listing|
+      occupied = listing.reservations.any? do |res|
+        c_in = intize(res.checkin.to_s)
+        c_out = intize(res.checkout.to_s)
+        c_in > start && c_in < stop || c_out > start && c_out < stop || c_in < start && c_out > stop
+      end
+      if occupied == false
+        emray << listing
+      end
+    end
+    emray
   end
 
 end
