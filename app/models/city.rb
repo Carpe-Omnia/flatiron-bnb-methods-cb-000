8946ef6784308da3a502677571ab0e20a72ebeb8
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-02-01 17:50:33 +0000

    Automatically backed up by Learn

--- app/models/city.rb
+++ app/models/city.rb
@@ -3,12 +3,13 @@ def initize (date)
   ring = ray.join
   num = ring.to_i
 end
+
 class City < ActiveRecord::Base
   has_many :neighborhoods
   has_many :listings, :through => :neighborhoods
 
 
-  def city_openings(start, stop)
+  def city_openings(startdate, stopdate)
     startray = start.split("-")
     stopray = stop.split("-")
     startray.each do |thing|
